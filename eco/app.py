cb346f0f907a16daeedcfcdf93609f19f8888ea4
ayu51591 <157950766+ayu51591@users.noreply.github.com>
2025-09-10 00:35:02 +0530

    todays work

--- eco/app.py
+++ eco/app.py
@@ -1,12 +1,16 @@
 from flask import request, Flask, jsonify
-import sqlite3
+import sqlite3, os
 from argon2 import PasswordHasher 
 app = Flask(__name__)
 
+SCAM_FOLDER = "eco/scam/"
+app.config["SCAM_FOLDER"] = SCAM_FOLDER
+
+global login_id
 global hashed
 ph = PasswordHasher()
 
-@app.route('/signup', methods=['POST'])#useke hisb se 
+@app.route('/Contact', methods=['POST'])#useke hisb se 
 def singup():
     data = request.get_json()
     name = data['name']
@@ -18,11 +22,11 @@ def singup():
     eco = sqlite3.connect("eco.db")
     cursor = eco.cursor()
      
-    cursor.execute("CREATE TABLE IF NOT EXISTS User(user_id INT PRIMARY KEY AUTOINCREMENT, name VARCHAR(15), contact VARCHAR(15), email VARCHAR(50), password VARCHAR(255), dob VARCHAR(20))")
+    cursor.execute("CREATE TABLE IF NOT EXISTS User(user_id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(15), contact VARCHAR(15), email VARCHAR(50), password VARCHAR(255), dob VARCHAR(20))")
     
     cursor.execute("INSERT INTO User(name, contact, email, password, dob) VALUES (?, ?, ?, ?, ?)",(name, contact, email, password, dob)) 
     
-    eco.commit() #eco close karni ha 
+    eco.commit() 
     eco.close()
 
     return jsonify({"message": "Signup successful"}), 201
@@ -32,12 +36,40 @@ def singup():
 def login():
     data = request.get_json()
     email = data['email']
-    password = ph.hash(data['password'])
+    password = data['password']
     
     eco = sqlite3.connect("eco.db")
     cursor = eco.cursor() 
     
-    row = cursor.execute("SELECT password FROM User WHERE email = ?", email)
-    if len(row) != 1 or :
-        return jsonify({"message": " Email not found"}), 404
-    elif row == password 
+    cursor.execute("SELECT password FROM User WHERE email = ?", email)
+    row = cursor.fetchone()
+    
+    eco.commit()
+    eco.close()
+    
+    if len(row) != 1:
+        return jsonify({"message": "Email not found"}), 404
+    elif ph.verify(row[0], password):
+        eco = sqlite3.connect("eco.db")
+        cursor = eco.cursor()
+        login_id = cursor.execute("SELECT user_id FROM User WHERE email = ?", email)
+        eco.commit()
+        eco.close()
+        return jsonify({"massage": "Login succesfully!"}), 200
+    else:
+        return jsonify({"message": "Password incorrect"}), 404
+
+@app.route('/Scam_Report',methods=['POST'])
+def scam():
+    data = request.get_json()
+    file = request.files["image"]
+    file_path = os.path.join(app.config["SCAM_FOLDER"], file.filename)#when u local host path is to be set then 
+    file.save(file_path)
+    title = data["title"]
+    discription = data["detail"]
+    
+    eco = sqlite3.connect("eco.db")
+    cursor = eco.cursor()
+     
+    cursor.execute("CREATE TABLE IF NOT EXISTS Scam_reports (report_id INTEGER PRIMARY KEY AUTOENCREMENT,  victim INTEGER, title TEXT, discription TEXT, image TEXT, FOREIGN KEY (user_id) RFREFENCES User(user_id)  ")
+    
